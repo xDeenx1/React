9d3bba174722af2c41e696900ac09b80359ada59
Denys Zhehlov <nordik2121@gmail.com>
2023-05-20 13:35:13 +0300

    Markup and basic services done

---
+++ src/services/MarvelService.js
@@ -0,0 +1,25 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/characters';
+    _apiKey = 'apikey=4564f847344360f1ae3e9c553d76d20a'
+
+    getResources = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, request status: ${res.status}`);
+        };
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResources(`${this._apiBase}?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResources(`${this._apiBase}/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
